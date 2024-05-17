e1360021133dfed7ecd58c5996434f8cb3fce43a
10210809 <10210809@B206PC21>
2024-05-17 09:27:03 +0300

    Добавьте файлы проекта.

---
+++ Clothing-store/items.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clothing_store
+{
+    internal class items
+    {
+        public static Entities1 entity;
+    }
+}
